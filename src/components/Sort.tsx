f231e633a291a2de8be769988550133d8c44277b
Shakhzod <147789264+shaxzod0022@users.noreply.github.com>
2025-05-03 10:17:52 +0500

    Update Sort.tsx

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { popularity } from "@/util/constants";
 import { style } from "@/util/style";
 
